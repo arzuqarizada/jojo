cb5d769a785bf6116c83a73118cc59b3451b2ac1
Arzu Ismailzada <arzuismailzada@Arzus-Air.fios-router.home>
2019-07-06 09:32:01 -0400

    hello world

---
+++ jojo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  jojo
+//
+//  Created by Arzu Ismailzada on 6/30/19.
+//  Copyright © 2019 Arzu Ismailzada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    print(hello world)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
